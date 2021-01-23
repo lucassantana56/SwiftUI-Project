63af880fba82a8823672c6dc998ea796e3acf1f7
Lucas Santana <44289738+lucassantana56@users.noreply.github.com>
2021-01-23 19:07:46 +0000

    Initial Commit

---
+++ mywords/mywordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mywordsApp.swift
+//  mywords
+//
+//  Created by Lucas Santana on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct mywordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
